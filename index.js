8e71c1f9bedb996ad5906276a59f38c701ef128b
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-22 01:13:52 +0300

    add file

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(number1 = 2000, number2 = 100) {
+    return (number1 / number2);
+}
+console.log(divide())
+
+function square(x) {
+    let square = (x)*x;
+    return square;
+}
+console.log(square())
+
+function add(a ,b) {
+    let add = (a+b);
+    return add;
+}
+console.log(add())
